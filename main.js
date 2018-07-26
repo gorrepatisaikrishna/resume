f6329eb8b75fdea59533d6c3df365405c4de7af7
gorrepatisaikrishna <gorrepatisaikrishnachowdary@gmail.com>
2018-07-26 16:04:16 +0530

    second version

--- main.js
+++ main.js
@@ -12,4 +12,39 @@ function loadjson(file,callback){
 loadjson("data.json",function(text){
 	let data=JSON.parse(text);
 	console.log(data);
+	basic(data.details);
+	edu(data.education)
 })
+
+var main=document.querySelector(".main");
+var left=document.createElement("div");
+left.classList.add("left");
+main.appendChild(left);
+function basic(basicdetails) {
+var image=document.createElement("img");
+image.src=basicdetails.image;
+left.appendChild(image);
+var name=document.createElement("h2");
+name.textContent=basicdetails.name;
+left.appendChild(name);
+var email=document.createElement("h2");
+email.textContent=basicdetails.email;
+left.appendChild(email);
+var phone=document.createElement("h2");
+phone.textContent=basicdetails.phone;
+left.appendChild(phone);
+}
+
+
+var right=document.createElement("div");
+right.classList.add("right");
+main.appendChild(right);
+function edu(education){
+	var un=document.createElement("ul");
+ right.appendChild(un);
+for (var i=0;i < education.length;i++){
+	var l=document.createElement("li");
+	l.textContent=education[i].course;
+	un.appendChild(l);
+}
+}
