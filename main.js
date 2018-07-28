ad251c1caeac80426383c0a4afbac42cad9b5422
gorrepatisaikrishna <gorrepatisaikrishnachowdary@gmail.com>
2018-07-28 12:32:05 +0530

    final update

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ loadjson("data.json",function(text){
 	let data=JSON.parse(text);
 	console.log(data);
 	basic(data.details);
-	edu(data.education)
+	carr(data.carrier);
+	edu(data.education);
+	skill(data.skills);
 })
 
 var main=document.querySelector(".main");
@@ -34,17 +36,56 @@ var phone=document.createElement("h2");
 phone.textContent=basicdetails.phone;
 left.appendChild(phone);
 }
-
-
 var right=document.createElement("div");
 right.classList.add("right");
 main.appendChild(right);
 function edu(education){
+	var e=document.createElement("h2");
+	e.textContent="Education Details";
+	right.appendChild(e)
 	var un=document.createElement("ul");
- right.appendChild(un);
+	right.appendChild(un);
 for (var i=0;i < education.length;i++){
 	var l=document.createElement("li");
+	var l=document.createElement("h3");
 	l.textContent=education[i].course;
 	un.appendChild(l);
+	var l2=document.createElement("li");
+	var l2=document.createElement("h3");
+	l2.textContent=education[i].college;
+	un.appendChild(l2);
+	var l3=document.createElement("li");
+	var l3=document.createElement("h3");
+	l3.textContent=education[i].percentage;
+	un.appendChild(l3);
 }
+}
+	function skill(skilldata) {
+		var s=document.createElement("div");
+		s.classList.add("sset");
+		right.appendChild(s);
+		var head=document.createElement("h2");
+		head.textContent="skills set";
+		
+		s.appendChild(document.createElement("HR"));
+		s.appendChild(head);
+		var t=document.createElement("table");
+		var tabledata="";
+		for (var i = 0; i < skilldata.length;i++){
+			tabledata+="<tr><td>"+skilldata[i].title+"</td><td>"+skilldata[i].output+"</td></tr>";
+			t.innerHTML=tabledata;
+		}
+		head.appendChild(t);
+	}
+
+function carr( carrier){
+	var d=document.createElement("div");
+	d.classList.add("carrier");
+    right.appendChild(d);
+    var e=document.createElement("h1");
+    e.textContent="carrier objective";
+    d.appendChild(e);
+    var para=document.createElement("p");
+    para.textContent=carrier.co;
+    e.appendChild(para);
 }
